49ddefa90314a540d0c1a02ca180f85064fdc677
Kazantseva Marina <kazmarin.91@gmail.com>
2017-06-24 10:38:49 +0400

    блок price

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,20 @@
-$(document).ready(function(){
-  // весь ваш код
+// $(document).ready(function(){
+//   // весь ваш код
+// });
+
+$(document).ready(function() {
+  function accordion() {
+
+    function openItem() {
+      if ($(this).hasClass('accordion__item-active')) {
+          $(this).removeClass('accordion__item-active');
+      } else {
+        $('.accordion__item').removeClass('accordion__item-active');
+        $(this).addClass('accordion__item-active');
+      }
+    }
+    $('.accordion__item').on('click', openItem);
+
+  }
+  accordion();
 });
